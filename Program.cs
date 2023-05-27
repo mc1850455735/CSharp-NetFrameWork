ab6650439c38330210621eebcb24b170d99e0186
majinliang <1850455735@qq.com>
2023-05-27 14:46:58 +0800

    添加了例14-3

--- Program.cs
+++ Program.cs
@@ -14,7 +14,7 @@ namespace CSharpCourse_NetFrameWork
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Example14_02());
+            Application.Run(new Example14_03());
         }
     }
 }
